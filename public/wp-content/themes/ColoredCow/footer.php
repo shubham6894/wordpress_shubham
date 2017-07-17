eda0f01e0c84ca9cb6b14861f78479446006d8b8
Shubham Nautiyal <shubham.nautiyal@coloredcow.com>
2017-07-17 16:44:47 +0530

    first commit resolved errors

--- public/wp-content/themes/ColoredCow/footer.php
+++ public/wp-content/themes/ColoredCow/footer.php
@@ -1,6 +1,5 @@
 <?php wp_footer(); ?>
 <div id="footer" class="fixed-bottom footer">
-	<hr>
 	<p>Copyright &copy; ColoredCow || 2017 </p>
 </div>
 </body>
--- public/wp-content/themes/ColoredCow/functions.php
+++ public/wp-content/themes/ColoredCow/functions.php
@@ -2,11 +2,9 @@
 
 if ( ! function_exists( 'cc_scripts' ) ) {
     function cc_scripts() {
-        wp_enqueue_script('cc-bootstrap', get_template_directory_uri().'/dist/lib/js/bootstrap.min.js', array('jquery'), '1.0.0', true);
+        wp_enqueue_script('cc-bootstrap4', get_template_directory_uri().'/dist/lib/js/bootstrap4.min.js', array('jquery'), '1.0.0', true);
         wp_enqueue_script('main', get_template_directory_uri().'/main.js', array('jquery', 'cc-bootstrap'), '1.0.0', true);
-        wp_enqueue_script('cc-bootstrap-script','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js');
         wp_enqueue_script('cc-custom-fontawesome','https://use.fontawesome.com/ffc2c94a85.js');
-        wp_enqueue_script('cc-jquery-script','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
         wp_localize_script( 'main', 'PARAMS', array('ajaxurl' => admin_url('admin-ajax.php')) );
 
     }
@@ -15,8 +13,7 @@ if ( ! function_exists( 'cc_scripts' ) ) {
 
 if ( ! function_exists( 'cc_styles' ) ) {
     function cc_styles() {  
-        wp_enqueue_style('cc-bootstrap', get_template_directory_uri().'/dist/lib/css/bootstrap.min.css');
-        wp_enqueue_style('cc-bootstrap-css','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css');
+        wp_enqueue_style('cc-bootstrap4', get_template_directory_uri().'/dist/lib/css/bootstrap4.min.css');
         wp_enqueue_style('cc-custom-font','https://fonts.googleapis.com/css?family=Oswald');
         wp_enqueue_style('style', get_template_directory_uri().'/style.css');
 
