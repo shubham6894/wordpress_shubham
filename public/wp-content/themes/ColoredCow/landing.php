9c97f4d1d4408e2cf5a46838975a9f6dfc9b91da
Shubham Nautiyal <shubham.nautiyal@coloredcow.com>
2017-07-18 10:32:37 +0530

    #5 show latest event

--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -4,9 +4,21 @@
  */
     get_header();
 ?>
-
-<?php
-?>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-8 col-lg-6">
+			</div>
+			<div class="col-sm-12 col-md-8 col-lg-6">
+				<div class="latest event">
+					Coming up...
+				</div>
+				<hr>
+			</div>
+		</div>
+	</div>
+	
+</body>
  
 <?php 
     get_footer(); 
