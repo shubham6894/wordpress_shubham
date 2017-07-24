e0ea6800b5bda28f0a05d1b6df59e450387673a1
Shubham Nautiyal <shubham.nautiyal@coloredcow.com>
2017-07-24 19:52:49 +0530

    requested changed done

--- public/wp-content/themes/ColoredCow/templates/content-requestmodal.php
+++ public/wp-content/themes/ColoredCow/templates/content-requestmodal.php
@@ -11,30 +11,30 @@
                         <input type="hidden" class="form-control" name="request_id" id="request_id">
                         <div class="form-group">
                             <label for="recipient-name" class="form-control-label">Your Name:</label>
-                            <input type="text" class="form-control" name="request_name" id="request_name" maxlength="30" required="required">  
+                            <input type="text" class="form-control" name="requested_guest_name" id="requested_guest_name" maxlength="30" required="required">  
                         </div>
                         <div class="form-group">
                             <label for="recipient-name" class="form-control-label">Your Email:</label>   
-                            <input type="email" class="form-control" placeholder="someone@example.com" name="request_emailid" id="request_emailid" maxlength="30" required="required"> 
+                            <input type="email" class="form-control" placeholder="someone@example.com" name="requested_guest_email" id="requested_guest_email" maxlength="30" required="required"> 
                         </div>
                         <div class="form-group">
                             <label for="recipient-number" class="form-control-label">Mobile Number:</label>
-                            <input type="number" class="form-control"  placeholder="10 digit mobile no." name="phonenumber" id="phonenumber" max="9999999999" required="required">    
+                            <input type="number" class="form-control"  placeholder="10 digit mobile no." name="requested_guest_number" id="requested_guest_number" max="9999999999" required="required">    
                         </div>  
                         <div class="form-group">
                             <label for="recipient-name" class="form-control-label">Gender:</label>
                             <label class="custom-control custom-radio">
-                                <input name="request_gender" value="Male" type="radio" class="custom-control-input" required="required">
+                                <input name="requested_guest_gender" value="Male" type="radio" class="custom-control-input" required="required">
                                 <span class="custom-control-indicator"></span>
                                 <span class="custom-control-description">Male</span>
                             </label>
                             <label class="custom-control custom-radio">
-                                <input name="request_gender" value="Female" type="radio" class="custom-control-input" required="required">
+                                <input name="requested_guest_gender" value="Female" type="radio" class="custom-control-input" required="required">
                                 <span class="custom-control-indicator"></span>
                                 <span class="custom-control-description">Female</span>
                             </label>
                             <label class="custom-control custom-radio">
-                                <input name="request_gender" value="Others" type="radio" class="custom-control-input" required="required">
+                                <input name="requested_guest_gender" value="Others" type="radio" class="custom-control-input" required="required">
                                 <span class="custom-control-indicator"></span>
                                 <span class="custom-control-description">Others</span>
                             </label>
